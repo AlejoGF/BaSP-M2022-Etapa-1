c4e49d5c23f266e5e6bd4bfc71d1a4b43fb3d2fa
alejogf <agonzalezfittipaldi@gmail.com>
2022-04-14 19:04:24 -0300

    BaSP-W05-2022: ex01-variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,23 @@
+// alert("Checking if this works");
+
+// variables and operators exercises
+// a.
+var numberOne = 21;
+var numberTwo = 7;
+var finalNumber = numberOne + numberTwo;
+
+console.log('Total =', finalNumber);
+
+// b.
+var cityBorn = 'Rosario';
+var countryBorn = 'Argentina';
+var placeBorn = cityBorn + ", " + countryBorn;
+console.log('Where are you from?: ', placeBorn);
+
+/// c.
+var firstName = 'Alejo';
+var lastName = 'Gonzalez';
+var lengthName;
+lengthName = firstName.length+ lastName.length;
+
+console.log(lengthName);
