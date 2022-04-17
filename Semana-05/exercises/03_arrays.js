8924cee772b20b94d10578480e07fdb4dd79b489
alejogf <agonzalezfittipaldi@gmail.com>
2022-04-17 17:25:14 -0300

    BaSP-W05-2022: ex04-if else

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -1,7 +1,5 @@
 console.log('\n--EXERCISE 3: ARRAYS');
 
-// BaSP-W05-2022: ex03-arrays
-
 // a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).
 console.log('-Exercise 3.a:');
 
